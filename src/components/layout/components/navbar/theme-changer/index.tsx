30cefd4bff2830aab713baf5f80372489caac622
azrux <lausofimarce@gmail.com>
2024-04-13 10:28:45 -0300

    fix: theme changer code fixes

--- src/components/layout/components/navbar/theme-changer/index.tsx
+++ src/components/layout/components/navbar/theme-changer/index.tsx
@@ -1,29 +1,27 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { Switch } from "@nextui-org/react";
 import { useTheme } from "next-themes";
+import type { FC } from "react";
 
-const ThemeChanger = () => {
+const ThemeChanger: FC = () => {
 	const { theme, setTheme } = useTheme();
 
 	return (
-		<div>
-			<Switch
-				className="animate-none"
-				size="md"
-				thumbIcon={({ isSelected, className }) =>
-					isSelected ? (
-						<span className={className}>
-							<Icon icon="material-symbols:sunny" />
-						</span>
-					) : (
-						<span className={className}>
-							<Icon icon="mdi:moon-waning-crescent" />
-						</span>
-					)
-				}
-				onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
-			/>
-		</div>
+		<Switch
+			size="md"
+			thumbIcon={({ isSelected, className }) =>
+				isSelected ? (
+					<span className={className}>
+						<Icon icon="material-symbols:sunny" />
+					</span>
+				) : (
+					<span className={className}>
+						<Icon icon="mdi:moon-waning-crescent" />
+					</span>
+				)
+			}
+			onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+		/>
 	);
 };
 
