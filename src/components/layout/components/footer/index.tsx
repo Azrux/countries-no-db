ee4acb911d273eca29617b38c0e885d7da4f18c5
azrux <lausofimarce@gmail.com>
2024-04-13 13:11:25 -0300

    feat: add footer

---
+++ src/components/layout/components/footer/index.tsx
@@ -0,0 +1,34 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { Tooltip } from "@nextui-org/react";
+import type { FC } from "react";
+import { Link } from "react-router-dom";
+
+const Footer: FC = () => {
+	return (
+		<div className="flex flex-col sm:flex-row justify-around items-center bg-default-50 fixed bottom-0 w-full border-t-1 border-default z-50">
+			<div className="flex items-center space-x-4 pb-2 sm:pb-0">
+				{/* TODO: Add pagination here? */}
+				<Tooltip content="Portfolio">
+					<Link to="https://portfolio-kype.onrender.com/" target="_blank">
+						<Icon icon="ph:code-bold" />
+					</Link>
+				</Tooltip>
+				<Tooltip content="Linkedin">
+					<Link
+						to="https://www.linkedin.com/in/laura-marcenaro/"
+						target="_blank"
+					>
+						<Icon icon="mdi:linkedin" />
+					</Link>
+				</Tooltip>
+				<Tooltip content="Github">
+					<Link to="https://github.com/Azrux" target="_blank">
+						<Icon icon="mdi:github" />
+					</Link>
+				</Tooltip>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,14 @@
 import type { FC } from "react";
 import type { LayoutProps } from "./types";
 import NavBar from "./components/navbar";
+import Footer from "./components/footer";
 
 const Layout: FC<LayoutProps> = ({ children, ...props }) => {
 	return (
 		<div {...props}>
 			<NavBar />
 			<div className="pb-40 sm:pb-16 px-4">{children}</div>
+			<Footer />
 		</div>
 	);
 };
